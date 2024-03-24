e18147d66465288971abcf8d7987436134d2d32f
Alejo Di Diego <alejoh2014@gmail.com>
2024-03-24 03:15:09 -0300

    14

--- frontend/src/app/layouts/GuestLayout/GuestLayout.tsx
+++ frontend/src/app/layouts/GuestLayout/GuestLayout.tsx
@@ -3,9 +3,9 @@ import NavBar from "./NavBar";
 
 const GuestLayout = () => {
   return (
-    <div className="w-full h-full pt-2 max-w-screen-2xl mx-auto">
+    <div className="w-full h-full pt-2">
       <NavBar />
-      <div className="mx-10 mt-10">
+      <div className="mt-10 max-w-screen-2xl mx-auto">
         <Outlet />
       </div>
     </div>
--- frontend/src/app/layouts/GuestLayout/NavBar.tsx
+++ frontend/src/app/layouts/GuestLayout/NavBar.tsx
@@ -7,21 +7,23 @@ const NavBar = () => {
   const handleClick = () => setIsOpen(!isOpen);
 
   return (
-    <div className="w-[99%] h-14 mx-auto rounded-lg border border-white flex items-center px-6 text-white justify-between relative">
-      <Link to="">
-        <h2 className="font-bold text-xl">PDF CONVERTER</h2>
-      </Link>
-      <button
-        className="bg-gray-100/20 px-2 p-1 rounded flex items-center gap-2 active:bg-gray-100/30 transition-all"
-        onClick={handleClick}
-      >
-        Tools
-        <img
-          src="play.png"
-          className={`w-2 text-white transition-all ${isOpen && "rotate-90"}`}
-        />
-      </button>
-      <Menu isOpen={isOpen} />
+    <div className="border border-white w-[99%] mx-auto px-6 rounded-lg">
+      <div className=" max-w-screen-2xl h-14 mx-auto flex items-center text-white justify-between relative">
+        <Link to="">
+          <h2 className="font-bold text-xl">PDF CONVERTER</h2>
+        </Link>
+        <button
+          className="bg-gray-100/20 px-2 p-1 rounded flex items-center gap-2 active:bg-gray-100/30 transition-all"
+          onClick={handleClick}
+        >
+          Tools
+          <img
+            src="play.png"
+            className={`w-2 text-white transition-all ${isOpen && "rotate-90"}`}
+          />
+        </button>
+        <Menu isOpen={isOpen} />
+      </div>
     </div>
   );
 };
